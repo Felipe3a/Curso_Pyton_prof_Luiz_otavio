f51ae5106691f8547392d0810b92347bea6af18f
Felipe3a <fpsdev3a@gmail.com>
2025-11-05 21:55:24 -0300

    aula24

---
+++ aula24/exercicio_variaveis.py
@@ -0,0 +1,12 @@
+nome = 'Felipe'
+autura = 1.75
+idade = 38
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+
+
+print('Nome:', nome)
+print('Ano de nascimento:', ano_nascimento)
+print('Altura:', autura)
+print('Idade:', idade)
+print('É maior de idade?', maior_de_idade)
